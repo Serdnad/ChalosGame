adbaf1b0576dc0c24fc3d0b06924ec0d938ddffa
Gonzalo Gonzalez <gonzalogonzalez2000@gmail.com>
2017-06-10 00:49:19 -0400

    added player script
    movement

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up"))
+            this.GetComponent<Rigidbody>().AddForce(transform.forward * 15);
+        else if (Input.GetKey("down"))
+            this.GetComponent<Rigidbody>().AddForce(-transform.forward * 15);
+        else if (Input.GetKey("left"))
+            this.GetComponent<Rigidbody>().AddForce(-transform.right * 15);
+        else if (Input.GetKey("right"))
+            this.GetComponent<Rigidbody>().AddForce(transform.right * 15);
+    }
+}
