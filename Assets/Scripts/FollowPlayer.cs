2fc90a5a4016a62f1c2b4a9dcae914da03480b6a
Serdnad <andresgutierrez535@gmail.com>
2017-06-10 22:27:15 -0500

    Updated FollowPlayer

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,13 +7,13 @@ public class FollowPlayer : MonoBehaviour {
     public GameObject player;       //Public variable to store a reference to the player game object
 
 
-    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private Vector3 offset = new Vector3(0, 10, -10); //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
     }
 
     // FixedUpdate is called through evenly spaced time intervals, regardless of framerate.
