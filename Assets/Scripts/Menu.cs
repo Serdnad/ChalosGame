5093fd7ce47c1ee29da4fb39b4e9ce0bbd0028fd
Gonzalo Gonzalez <gonzalogonzalez2000@gmail.com>
2017-06-16 00:39:15 -0400

    Menu Addition
    also fixed playerdeath explosion

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public Canvas mainmenu;
+    public Canvas levelselect;
+
+    void Awake()
+    {
+        levelselect.enabled = false;
+    }
+
+    public void levelselectOn()
+    {
+        mainmenu.enabled = false;
+        levelselect.enabled = true;
+    }
+
+    public void levelselectBack()
+    {
+        mainmenu.enabled = true;
+        levelselect.enabled = false;
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void level1()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void level2()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+
+    public void level3()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+}
--- Assets/Scripts/ResetOnDeath.cs
+++ Assets/Scripts/ResetOnDeath.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ResetOnDeath : MonoBehaviour {
-
+    public GameObject playerdeath;
 	// Use this for initialization
 	void Start () {
+
+        GetComponent<Rigidbody>().AddExplosionForce(20, playerdeath.transform.position, 2, 0);
         Invoke("ResetLevel", 3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
 
     // Resets the level
     void ResetLevel() {
